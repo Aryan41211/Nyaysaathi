57715e2bd61607fd2e50ffa006c0715675e0fbe6
Aryan Kondekar <aryankondekar16@gmail.com>
2026-03-29 15:07:40 +0530

    fix routing debug

--- nyayasaathi/urls.py
+++ nyayasaathi/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.http import HttpResponse
+from django.urls import include, path
 
 urlpatterns = [
+    path('', lambda request: HttpResponse("ROOT WORKING")),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
